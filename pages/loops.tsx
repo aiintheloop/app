2fc8895fde1ef31d16228844348d70fd7b264605
nabilfatih <nabilakbarazzima@gmail.com>
2023-02-13 11:14:50 +0100

    add dropdown menu config

--- pages/loops.tsx
+++ pages/loops.tsx
@@ -6,7 +6,7 @@ import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import { Listbox } from '@headlessui/react';
+import { Listbox, Menu } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 import { TextField } from '@mui/material';
 import { Dialog, Transition } from '@headlessui/react';
@@ -39,10 +39,55 @@ function Card({ title, description, footer, children }: Props) {
         {title && (
           <div className="flex align-middle content-center justify-between">
             <h3 className="text-2xl mb-1 font-medium">{title}</h3>
-            <SettingsOutlinedIcon
-              fontSize="medium"
-              className="hover:cursor-pointer"
-            />
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center rounded-md text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  <SettingsOutlinedIcon
+                    fontSize="medium"
+                    className="hover:cursor-pointer text-zinc-900"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-20 origin-top-right divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active ? 'bg-zinc-500 text-white' : 'text-zinc-900'
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          Edit
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active ? 'bg-zinc-500 text-white' : 'text-zinc-900'
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         )}
         {description && (
