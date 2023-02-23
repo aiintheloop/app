7a66be577f31215848c65018b01c6e6f6c266c52
Thomas Weiland <thomasweiland93@gmail.com>
2023-02-23 18:47:45 +0100

    Change logo

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,20 +1,29 @@
+
+const cls1 = {
+  fillRule: 'evenodd', // note the capital 'W' here
+  msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+};
+
 const Logo = ({ className = '', ...props }) => (
   <svg
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
+    width="60"
+    height="60"
+    viewBox="0 0 250 500"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     className={className}
     {...props}
   >
-    <rect width="100%" height="100%" rx="16" fill="white" />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="black"
-    />
+
+    <path id="Farbfüllung_6" data-name="Farbfüllung 6" style={{fillRule: "evenodd", fill: "#008dd5"}}
+          d="M342,242l11,1c4.551,5.754,8.063,6.81,8,18-5.4,6.772-6.185,11.179-19,11-6.772-5.4-11.179-6.185-11-19,1.879-2.255,2.674-6.233,5-8C337.855,243.591,340.29,243.43,342,242Z" />
+    <path id="Farbfüllung_5" data-name="Farbfüllung 5" style={{fillRule: "evenodd",fill: "#e43f6f"}}
+          d="M166,242l11,1c4.551,5.754,8.063,6.81,8,18-5.4,6.772-6.185,11.179-19,11-6.772-5.4-11.179-6.185-11-19,1.879-2.255,2.674-6.233,5-8C161.855,243.591,164.29,243.43,166,242Z" />
+    <path id="Farbfüllung_1" data-name="Farbfüllung 1" style={{fillRule: "evenodd", fill: "#000000"}}
+          d="M360,228H330c0.8-28.381,1.717-62.158-9-80-23.68-39.421-103.359-39.6-127,0-10.691,17.907-9.781,51.569-9,80H155C151.067,44.16,363.782,44.033,360,228Z" />
+    <path id="Farbfüllung_2" data-name="Farbfüllung 2" style={{fillRule: "evenodd", fill: "#000000"}}
+          d="M155,290h30c-0.793,28.1-1.6,61.347,9,79,23.68,39.421,103.359,39.6,127,0,10.578-17.717,9.778-50.853,9-79h30C364,472.7,151.163,472.837,155,290Z" />
+
   </svg>
 );
 
