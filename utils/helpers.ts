be9171774ac6750f71c9563c72059183796190e7
nabilfatih <nabilakbarazzima@gmail.com>
2023-02-27 09:32:17 +0100

    add is valid http url helpers

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -63,3 +63,12 @@ export const capitalizeFirstLetter = (word: string) => {
   // Directly return the joined string
   return splitStr.join(' ');
 };
+
+export function isValidHttpUrl(string: string) {
+  try {
+    const url = new URL(string);
+    return url.protocol === 'http:' || url.protocol === 'https:';
+  } catch (err) {
+    return false;
+  }
+}
