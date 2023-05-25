7ad0e1019c2223affd9774744d41d4207266c02d
nabilfatih <nabilakbarazzima@gmail.com>
2023-05-25 13:38:47 +0200

    update style editable popup approvals

--- components/ui/Modal/EditablePopup.tsx
+++ components/ui/Modal/EditablePopup.tsx
@@ -1,11 +1,6 @@
-import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import DialogContentText from '@mui/material/DialogContentText';
+import React, { useState, Fragment } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
 
 interface EditablePopupProps {
   data: Record<string, string>;
@@ -41,34 +36,103 @@ const EditablePopup: React.FC<EditablePopupProps> = ({
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Edit Data</DialogTitle>
-      <DialogContent className="flex flex-col gap-2">
-        <DialogContentText>
-          Edit the prompts below and click Save to reloop.
-        </DialogContentText>
-        {Object.entries(editableData).map(([key, value]) => (
-          <TextField
-            key={key}
-            label={key}
-            value={value}
-            onChange={(event) => handleFieldChange(key, event.target.value)}
-            fullWidth
-            className="w-full"
-          />
-        ))}
-      </DialogContent>
-      <DialogActions>
-        <div className="grid grid-cols-2 w-48 gap-2 mx-auto">
-          <button className="btn btn-md btn-accent" onClick={handleCancelClick}>
-            Cancel
-          </button>
-          <button className="btn btn-md btn-success" onClick={handleSaveClick}>
-            Save
-          </button>
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-base-300 bg-opacity-25 backdrop-blur-sm" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-neutral p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title as="h3" className="text-lg pb-4 leading-6">
+                  Edit Data
+                </Dialog.Title>
+                <span className="text-sm">
+                  Edit the prompts below and click Save to reloop.
+                </span>
+                <section className="my-2 mx-auto">
+                  <section>
+                    {Object.entries(editableData).map(([key, value]) => (
+                      <textarea
+                        key={key}
+                        value={value}
+                        onChange={(event) =>
+                          handleFieldChange(key, event.target.value)
+                        }
+                        placeholder={key}
+                        className="textarea textarea-primary w-full"
+                      />
+                    ))}
+                  </section>
+                </section>
+                <div className="mt-4">
+                  <div className="grid grid-cols-2 w-48 gap-2 mx-auto">
+                    <button
+                      className="btn btn-md btn-error"
+                      onClick={handleCancelClick}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      className="btn btn-md btn-success"
+                      onClick={handleSaveClick}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </div>
-      </DialogActions>
-    </Dialog>
+      </Dialog>
+    </Transition>
+    // <Dialog open={open} onClose={onClose}>
+    //   <DialogTitle>Edit Data</DialogTitle>
+    //   <DialogContent className="flex flex-col gap-2">
+    //     <DialogContentText>
+    //       Edit the prompts below and click Save to reloop.
+    //     </DialogContentText>
+    //     {Object.entries(editableData).map(([key, value]) => (
+    //       <TextField
+    //         key={key}
+    //         label={key}
+    //         value={value}
+    //         onChange={(event) => handleFieldChange(key, event.target.value)}
+    //         fullWidth
+    //         className="w-full"
+    //       />
+    //     ))}
+    //   </DialogContent>
+    //   <DialogActions>
+    //     <div className="grid grid-cols-2 w-48 gap-2 mx-auto">
+    //       <button className="btn btn-md btn-accent" onClick={handleCancelClick}>
+    //         Cancel
+    //       </button>
+    //       <button className="btn btn-md btn-success" onClick={handleSaveClick}>
+    //         Save
+    //       </button>
+    //     </div>
+    //   </DialogActions>
+    // </Dialog>
   );
 };
 
