05836a9cd05d0908cbf61e50f4d703f6b57b5cd2
Thomas Weiland <thomasweiland93@gmail.com>
2023-07-24 22:07:51 +0200

    Zapier Exmaple

---
+++ pages/api/zapier-example.ts
@@ -0,0 +1,17 @@
+import { supabase } from '@/utils/supabase-client';
+import withAuth from '@/utils/withAuth';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+
+const zapierExample = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    result: [{
+      "data": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    }, {
+      "data": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    }],
+  });
+};
+
+export default withAuth(zapierExample);
