9b3647671797a6716f1f369bfd87641d78c269a8
nabilfatih <nabilakbarazzima@gmail.com>
2023-08-02 15:44:06 +0200

    update meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,6 @@ export default function Layout({ children, meta: pageMeta }: Props) {
   const meta = {
     title: 'AI2Loop',
     description: 'Brought to you by Tec2Scale',
-    cardImage: '/og.png',
     ...pageMeta
   };
 
@@ -32,12 +31,13 @@ export default function Layout({ children, meta: pageMeta }: Props) {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.cardImage} />
+        {/* <meta property="og:image" content={meta.cardImage} /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@vercel" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.cardImage} />
+        {/* <meta name="twitter:image" content={meta.cardImage} /> */}
+        <link rel="canonical" href={`https://ai2loop.com${router.asPath}`} />
       </Head>
       <NextNProgress
         color="#38BDF8"
