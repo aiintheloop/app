cd1d83cbbca183203141e2d03199808da44c9d03
Thomas Weiland <thomasweiland93@gmail.com>
2023-08-22 21:25:31 +0200

    Change accounts page according Dominiks changes

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,17 +1,13 @@
-import Link from 'next/link';
 import { ChangeEvent, ReactNode, useEffect, useState } from 'react';
 
 import LoadingDots from 'components/ui/LoadingDots';
-import Button from 'components/ui/Button';
 import { useUser } from 'utils/useUser';
 import { generateNewApiKey, getApiKey, postData } from 'utils/helpers';
 
 import { User } from '@supabase/supabase-js';
 import { withPageAuth } from '@supabase/auth-helpers-nextjs';
-import { TextField } from '@mui/material';
 import { getURL } from '@/utils/helpers';
-import { getUserDetails, supabase } from '@/utils/supabase-client';
-import { v4 as uuidv4 } from 'uuid';
+import { getUserDetails } from '@/utils/supabase-client';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
@@ -29,13 +25,13 @@ interface Props {
 
 function Card({ title, description, footer, children }: Props) {
   return (
-    <div className="card bg-base-200 max-w-3xl w-full p rounded-md m-auto my-8">
-      <div className="px-5 py-4">
-        <h3 className="text-2xl mb-1 font-medium">{title}</h3>
+    <div className='card bg-base-200 max-w-3xl w-full p rounded-md m-auto my-8'>
+      <div className='px-5 py-4'>
+        <h3 className='text-2xl mb-1 font-medium'>{title}</h3>
         <p>{description}</p>
         {children}
       </div>
-      <div className="px-5 py-3 rounded-b-md bg-base-300 shadow-inner">
+      <div className='px-5 py-3 rounded-b-md bg-base-300 shadow-inner'>
         {footer}
       </div>
     </div>
@@ -153,31 +149,31 @@ export default function Account({ user }: { user: User }) {
     }).format((subscription?.prices?.unit_amount || 0) / 100);
 
   return (
-    <section className="mb-32">
-      <div className="max-w-6xl mx-auto pt-4 sm:pt-12 pb-4 px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:flex-col sm:align-center">
-          <h1 className="text-4xl font-extrabold sm:text-center sm:text-6xl">
+    <section className='mb-32'>
+      <div className='max-w-6xl mx-auto pt-4 sm:pt-12 pb-4 px-4 sm:px-6 lg:px-8'>
+        <div className='sm:flex sm:flex-col sm:align-center'>
+          <h1 className='text-4xl font-extrabold sm:text-center sm:text-6xl'>
             Account
           </h1>
-          <p className="mt-5 text-xl sm:text-center sm:text-2xl max-w-2xl m-auto">
-            We partnered with Stripe for a simplified billing.
+          <p className='mt-5 text-xl sm:text-center sm:text-2xl max-w-2xl m-auto'>
+            Your personal ai2loop settings
           </p>
         </div>
       </div>
-      <div className="p-4">
+      <div className='p-4'>
         <ApiKeyModal
           generateApiKey={regenerateApiKey}
           modalOpen={apiKeyModalOpen}
           setModalOpen={setApiKeyModalOpen}
         />
         {/** <Card
-          title="Your Plan"
-          description={
+         title="Your Plan"
+         description={
             subscription
               ? `You are currently on the ${subscription?.prices?.products?.name} plan.`
               : ''
           }
-          footer={
+         footer={
             <div className="flex items-start justify-between flex-col sm:flex-row sm:items-center">
               <p className="pb-4 sm:pb-0">
                 Manage your subscription on Stripe.
@@ -193,9 +189,9 @@ export default function Account({ user }: { user: User }) {
               </button>
             </div>
           }
-        >
-          <div className="text-xl mt-8 mb-4 font-semibold">
-            {isLoading ? (
+         >
+         <div className="text-xl mt-8 mb-4 font-semibold">
+         {isLoading ? (
               <div className="h-12 mb-6">
                 <LoadingDots />
               </div>
@@ -206,23 +202,23 @@ export default function Account({ user }: { user: User }) {
                 <a>Choose your plan</a>
               </Link>
             )}
-          </div>
-        </Card>**/}
+         </div>
+         </Card>**/}
         <Card
-          title="Your Email"
-          description="Please enter the email address you want to use to login."
-          footer={<p>We will email you to verify the change.</p>}
+          title='Your Email'
+          description='Your current email address'
+          footer={''}
         >
-          <p className="text-xl mt-8 mb-4 font-semibold">
+          <p className='text-xl mt-8 mb-4 font-semibold'>
             {user ? user.email : undefined}
           </p>
         </Card>
         <Card
-          title="Your API Key"
-          description="Please keep this Key confidential."
+          title='Your API Key'
+          description='Please keep this Key confidential.'
           footer={
-            <div className="flex items-start justify-between flex-col sm:flex-row sm:items-center">
-              <p>Your Api Key</p>
+            <div className='flex items-start justify-between flex-col sm:flex-row sm:items-center'>
+              <p>Your API Key</p>
               <button
                 className={'btn btn-primary btn-md'}
                 onClick={() => setApiKeyModalOpen(true)}
@@ -232,9 +228,9 @@ export default function Account({ user }: { user: User }) {
             </div>
           }
         >
-          <div className="text-xl mt-8 mb-4 font-semibold">
+          <div className='text-xl mt-8 mb-4 font-semibold'>
             {userDetails ? (
-              <div className="flex item-center justify-between">
+              <div className='flex item-center justify-between'>
                 <p>
                   {hideApiKey ? '*********************' : apiKey ? apiKey : ''}
                 </p>
@@ -259,49 +255,49 @@ export default function Account({ user }: { user: User }) {
                 </div>
               </div>
             ) : (
-              <div className="h-8 mb-6">
+              <div className='h-8 mb-6'>
                 <LoadingDots />
               </div>
             )}
           </div>
         </Card>
         <Card
-          title="Slack Integration"
-          description="Please authenticate with Slack if you want the Approvals in your slack channel"
+          title='Slack Integration'
+          description='Please authenticate with Slack if you want the Approvals in your slack channel'
           footer={
-            <div className="flex items-start justify-between flex-col sm:flex-row sm:items-center">
+            <div className='flex items-start justify-between flex-col sm:flex-row sm:items-center'>
               <p>Repress the button if you want to change the Channel</p>
               <p>
                 <a
                   href={`https://slack.com/oauth/v2/authorize?scope=incoming-webhook&redirect_uri=${encodeURIComponent(
                     getURL() + 'api/integrate_slack'
                   )}&client_id=${SLACK_CLIENT_ID}`}
-                  className="btn btn-primary btn-md"
+                  className='btn btn-primary btn-md'
                 >
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns='http://www.w3.org/2000/svg'
                     style={{
                       height: '20px',
                       width: '20px',
                       marginRight: '12px'
                     }}
-                    viewBox="0 0 122.8 122.8"
+                    viewBox='0 0 122.8 122.8'
                   >
                     <path
-                      d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
-                      fill="#e01e5a"
+                      d='M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z'
+                      fill='#e01e5a'
                     ></path>
                     <path
-                      d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
-                      fill="#36c5f0"
+                      d='M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z'
+                      fill='#36c5f0'
                     ></path>
                     <path
-                      d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
-                      fill="#2eb67d"
+                      d='M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z'
+                      fill='#2eb67d'
                     ></path>
                     <path
-                      d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
-                      fill="#ecb22e"
+                      d='M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z'
+                      fill='#ecb22e'
                     ></path>
                   </svg>
                   Add to Slack
@@ -311,67 +307,68 @@ export default function Account({ user }: { user: User }) {
           }
         ></Card>
         <Card
-          title="Discord Integration"
-          description="Please enter a Discord Webhook"
+          title='Discord Integration'
+          description='Please enter a Discord Webhook'
           footer={
-            <p>
-              <a
-                href={
-                  'https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
-                }
+            <p className='mt-5 flex items-center flex-row mb-4 font-semibold'>
+              <input
+                aria-label='Discord Webhook'
+                type='text'
+                placeholder='Discord Webhook'
+                className='input input-bordered input-primary w-full'
+                onChange={handleDiscordWebhookChange}
+                value={discordWebhook}
+              />
+              <button
+                onClick={handleDiscordIntegration}
+                className={'ml-5 btn btn-primary'}
               >
-                More Infos
-              </a>
+                Add to Discord
+              </button>
             </p>
           }
         >
-          <p className="mt-5 flex items-center flex-row mb-4 font-semibold">
-            <input
-              aria-label="Discord Webhook"
-              type="text"
-              placeholder="Discord Webhook"
-              className="input input-bordered input-primary w-full"
-              onChange={handleDiscordWebhookChange}
-              value={discordWebhook}
-            />
-            <button
-              onClick={handleDiscordIntegration}
-              className={'ml-5 btn btn-primary'}
+          <p>
+            <a
+              href={
+                'https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
+              }
             >
-              Add to Discord
-            </button>
+              More Infos
+            </a>
           </p>
         </Card>
         <Card
-          title="Teams Integration"
-          description="Please enter a Teams Webhook"
+          title='Teams Integration'
+          description='Please enter a Teams Webhook'
           footer={
-            <p>
-              <a
-                href={
-                  'https://learn.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook?tabs=dotnet'
-                }
+
+            <p className='mt-5 flex items-center flex-row mb-4 font-semibold'>
+              <input
+                aria-label='Teams Webhook'
+                type='text'
+                placeholder='Teams Webhook'
+                className='input input-bordered input-primary w-full'
+                onChange={handleTeamsWebhookChange}
+                value={teamsWebhook}
+              />
+              <button
+                onClick={handleTeamsIntegration}
+                className={'ml-5 btn btn-primary'}
               >
-                More Infos
-              </a>
+                Add to Teams
+              </button>
             </p>
           }
         >
-          <p className="mt-5 flex items-center flex-row mb-4 font-semibold">
-            <input
-              aria-label="Teams Webhook"
-              type="text"
-              placeholder="Teams Webhook"
-              className="input input-bordered input-primary w-full"
-              onChange={handleTeamsWebhookChange}
-              value={teamsWebhook}
-            />
-            <button
-              onClick={handleTeamsIntegration}
-              className={'ml-5 btn btn-primary'}
+          <p>
+            <a
+              href={
+                'https://learn.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook?tabs=dotnet'
+              }
             >
-              Add to Teams
-            </button>
+              More Infos
+            </a>
           </p>
         </Card>
       </div>
